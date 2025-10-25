39cd396f4d5add42cea1981169a8bbeb08aaa836
Bani <bbaltawalbeh23@cit.just.edu.jo>
2025-10-25 20:51:27 +0300

    MIT License

--- app/[username]/layout.js
+++ app/[username]/layout.js
@@ -8,7 +8,7 @@ export function generateStaticParams() {
 }
 
 // Disable dynamic params - users should be accessed via hash routing (e.g., /#username)
-// This is necessary for GitHub Pages static export
+
 export const dynamicParams = false;
 
 export default function UsernameLayout({ children }) {
